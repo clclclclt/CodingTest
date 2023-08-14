1f28a4ff8737c1c2cb290fd9c2988eeea855857a
clclclclt <129553579+clclclclt@users.noreply.github.com>
2023-08-14 14:36:23 +0900

    [Bronze V] Title: A+B - 4, Time: 228 ms, Memory: 17880 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,25 @@
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		Scanner sc=new Scanner(System.in);
+		
+		while(sc.hasNextInt()){
+		
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			System.out.println(a+b);
+		
+		}	
+		sc.close();
+		}
+}
+
+
