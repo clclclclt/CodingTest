462890e34af3d6b90bd9919abc7542b4c1bdbbc2
clclclclt <129553579+clclclclt@users.noreply.github.com>
2023-10-05 15:24:29 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String num = String.valueOf(n);
+        for(int i=0; i<num.length();i++){
+            answer+= (int)num.charAt(i) - '0';
+        }
+        return answer;
+    }
+}
