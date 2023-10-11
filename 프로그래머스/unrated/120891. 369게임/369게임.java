1c322015af3e5592321b88b21c4443afb5491abe
clclclclt <129553579+clclclclt@users.noreply.github.com>
2023-10-11 17:45:43 +0900

    [level unrated] Title: 369게임, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120891. 369게임/369게임.java
+++ 프로그래머스/unrated/120891. 369게임/369게임.java
@@ -3,8 +3,9 @@ class Solution {
         int answer = 0;
         String str = Integer.toString(order);
         for(int i=0; i<str.length();i++){
-            if(str.charAt(i)-'0' == 3 || str.charAt(i)-'0' == 
-               6 || str.charAt(i)-'0' ==9){
+            char c = str.charAt(i);
+            if(c-'0' == 3 || c-'0' == 
+               6 || c-'0' ==9){
                 answer++;
             }
                 
